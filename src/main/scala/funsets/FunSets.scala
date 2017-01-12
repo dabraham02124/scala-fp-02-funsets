6cb2daaffefcca5e3324129c5d0ea5410b830fc8
Daniel Abraham <dabraham@sweatshop.org>
2017-01-11 19:36:37 -0500

    everything works, but exists isn't defined in terms of forall.  It's weird that map changes behavior and exists doesn't when exists is the function that was changed...

--- src/main/scala/funsets/FunSets.scala
+++ src/main/scala/funsets/FunSets.scala
@@ -66,7 +66,15 @@ object FunSets {
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = forall(p, s)
+//    def exists(s: Set, p: Int => Boolean): Boolean = forall(p, s)
+    def exists(s: Set, p: Int => Boolean): Boolean = {
+    def iter(a: Int): Boolean = {
+      if (a > bound) false
+      else if (contains(s,a)) p(a) || iter(a+1)
+      else iter(a+1)
+    }
+    iter(-bound)
+  }
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
